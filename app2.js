f1bb3ab6c8b796636b2c39f8afa6931ff407540d
Uira Oliveira <uira.sampaio@hotmail.com>
2019-06-25 16:08:18 -0300

    testing background

--- app2.js
+++ app2.js
@@ -1,7 +1,10 @@
+// main Canvas star and stop functions
+
 const canvasGame = {
   canvasBox: document.getElementById('canvas'),
   canvas: document.createElement("canvas"),
   frames: 0,
+  maxEnemies: 2,
   start: function () {
     this.canvas.width = 800;
     this.canvas.height = 500;
@@ -11,44 +14,61 @@ const canvasGame = {
   },
   clear: function () {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  },
+  stop: function () {
+    clearInterval(interval);
   }
 }
 
-let characterRight = new Image();
-characterRight.src = 'images/right-side.png';
-let characterLeft = new Image();
-characterLeft.src = 'images/left-side.png';
-let characterDown = new Image();
-characterDown.src = 'images/knee.png';
-let attackImg = new Image();
-attackImg.src = 'images/attack-position.png';
-let downLeft = new Image();
-downLeft.src = 'images/knee-left.png';
-let attackImgLeft = new Image();
-attackImgLeft.src = 'images/attack-position-left.png';
+let bgTest = new Image();
+bgTest.src = 'images/super-mario-background-images-5632345.jpg';
 
+const backgroundImage = {
+  img: bgTest,
+  x: 0,
+  speed: -1,
+
+  move() {
+    this.x += this.speed;
+    this.x %= canvasGame.canvas.width;
+  },
+
+  draw() {
+    canvasGame.ctx.drawImage(this.img, this.x, 0);
+    if (this.speed < 0) {
+      canvasGame.ctx.drawImage(this.img, this.x + canvasGame.canvas.width, 0);
+    } else {
+      canvasGame.ctx.drawImage(this.img, this.x - this.img.width, 0);
+    }
+  },
+};
+
+
+let attackPattern = new Image();
+attackPattern.src = 'images/attack-pattern1.png';
+
+// change to player js archive
 
 class Player {
   constructor(x, y, health, dmg) {
     this.x = x;
     this.y = y;
     this.vx = 1;
-    this.spriteX = 0;
     this.speedX = 0;
     this.health = health;
     this.dmg = dmg;
     this.width = 29;
     this.height = 31;
-    this.frameIndex = 0;
     this.tickCount = 0;
     this.frames = 0;
     this.scaleWidth = this.width * 2;
     this.scaleHeight = this.height * 2;
-    this.otherFrame = 3;
     this.right = true;
     this.left = false;
     this.down = false;
     this.attack = false;
+    this.gunReload = true;
+    this.damageInterval = false;
   }
 
   // Used to draw animations with no modification on the x or y axis
@@ -70,6 +90,8 @@ class Player {
   render() {
     canvasGame.ctx.clearRect(this.x, this.y, this.width, this.height);
     if (this.right === true && this.down === true) {
+      const characterDown = new Image();
+      characterDown.src = 'images/knee.png';
       canvasGame.ctx.drawImage(
         characterDown,
         this.width * this.frames,
@@ -82,6 +104,8 @@ class Player {
         this.scaleHeight,
       );
     } else if (this.left === true && this.down === true) {
+      const downLeft = new Image();
+      downLeft.src = 'images/knee-left.png';
       canvasGame.ctx.drawImage(
         downLeft,
         this.width * this.frames,
@@ -94,12 +118,21 @@ class Player {
         this.scaleHeight,
       );
     } else if (this.left === true && this.attack === true) {
+      const attackImgLeft = new Image();
+      attackImgLeft.src = 'images/attack-position-left.png';
       this.characterAnimation(attackImgLeft);
     } else if (this.right === true && this.attack === true) {
+      const attackImg = new Image();
+      attackImg.src = 'images/attack-position.png';
       this.characterAnimation(attackImg);
     } else if (this.left === true) {
+      const characterLeft = new Image();
+      characterLeft.src = 'images/left-side.png';
       this.characterAnimation(characterLeft);
     } else if (this.right === true) {
+      const characterRight = new Image();
+      characterRight.src = 'images/right-side.png';
+
       this.characterAnimation(characterRight);
     }
   }
@@ -109,7 +142,7 @@ class Player {
     if (this.x + this.speedX <= 0) { this.x -= this.speedX; }
 
     this.x += this.speedX;
-    if (this.tickCount > 3) {
+    if (this.tickCount >= 3) {
       this.tickCount = 0;
       this.frames += 1;
     }
@@ -118,37 +151,192 @@ class Player {
     }
   }
 
-  attack() {
+  receiveDamage() {
+    console.log(this.health);
+    if (this.checkGameOver()) {
+      canvasGame.stop();
+    }
 
+    if (!this.damageInterval) {
+      this.health = this.health - 25;
+      this.x -= 1;
+      this.damageInterval = true;
+      setTimeout(() => this.damageInterval = false, 300);
+    }
   }
 
-  receiveDamage(dmg) {
-    this.health = this.health - dmg;
+  checkGameOver() {
+    return (this.health <= 0);
+  }
+
+  playerPositionRight() {
+    return this.x + this.width;
+  }
+
+  playerPositionLeft() {
+    return this.x;
+  }
+
+  crashWith(enemies) {
+    return !(this.playerPositionRight() < enemies.playerPositionLeft()
+      || this.playerPositionLeft() > enemies.playerPositionRight());
   }
 }
 
-// enemies
+// should be created with the game start function
+let player = new Player(50, 300, 100, 50);
+
+
+// should move to enemy javascript archive;
+let enemies = [];
+
 class Enemy {
-  constructor (x, y, health, dmg) {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.health;
-    this.dmg;
+    this.health = 100;
+    this.dmg = 25;
+    this.width = 20;
+    this.height = 20;
+  }
+
+  updateEnemy() {
+    canvasGame.ctx.fillStyle = 'red';
+    canvasGame.ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  takeDamage() {
+    this.health -= 25;
+    this.x += 10;
+  }
+
+  playerPositionRight() {
+    return this.x;
+  }
+
+  playerPositionLeft() {
+    return this.x - this.width;
+  }
+
+  crashWith(shot) {
+    return !(this.playerPositionRight() < shot.shotPositionLeft()
+      || this.playerPositionLeft() > shot.shotPositionRight());
   }
 }
 
+// will need to update to check the damage of the obstacle
+function updateEnemies() {
+  enemies.forEach((enemy, idx) => {
+    if (enemy.health <= 0) {
+      enemies.splice(idx, 1);
+    }
+  });
 
-let player = new Player(50, 300, 100, 50);
+  for (let i = 0; i < enemies.length; i += 1) {
+    if (enemies[i].x < player.x) {
+      enemies[i].x += 1;
+    } else if (enemies[i].x > player.x) {
+      enemies[i].x -= 1;
+    }
+    enemies[i].updateEnemy();
+  }
+
+  canvasGame.frames += 1;
+  if (enemies.length < canvasGame.maxEnemies) {
+    if (canvasGame.frames % 250 === 0) {
+      enemies.push(new Enemy(900, 300));
+    }
+  }
+}
+
+function checkGameOver() {
+  const crashed = enemies.some(dmg => player.crashWith(dmg));
+
+  if (crashed) {
+    player.receiveDamage();
+  }
+}
+
+let laserArr = [];
+
+function checkCrash() {
+  enemies.forEach((item) => {
+    const test = laserArr.some((dmg, idx) => {
+      if (item.crashWith(dmg)) {
+        laserArr.splice(idx, 1);
+        return item.crashWith(dmg);
+      }
+    });
+    if (test) {
+      item.takeDamage();
+    }
+  });
+}
+
+class Shot {
+  constructor(side) {
+    this.x = player.x + 65;
+    this.y = player.y + 25;
+    this.trajectorie = side;
+    this.charge = 1;
+  }
+
+  update() {
+    if (this.trajectorie === true) {
+      canvasGame.ctx.beginPath();
+      canvasGame.ctx.arc(this.x, this.y, 5, 0, 2 * Math.PI);
+      canvasGame.ctx.fill();
+    } else {
+      canvasGame.ctx.beginPath();
+      canvasGame.ctx.arc(this.x - 65, this.y, 5, 0, 2 * Math.PI);
+      canvasGame.ctx.fill();
+    }
+  }
+
+  shotPositionRight() {
+    return this.x;
+  }
+
+  shotPositionLeft() {
+    return this.x;
+  }
+
+  montersHit() {
+    this.charge -= 1;
+  }
+}
+
 canvasGame.start();
 player.render();
+
+function updateShot() {
+  laserArr.forEach((shot, idx) => {
+    shot.update();
+    if (shot.charge <= 0) {
+      laserArr.splice(idx, 1);
+    }
+    if (shot.trajectorie === true) {
+      shot.x += 1;
+    } else {
+      shot.x -= 1;
+    }
+
+    if (shot.x > player.x + 300 || shot.x < player.x - 300) {
+      laserArr.splice(idx, 1);
+    }
+  });
+}
 
 
 function gameLoop() {
   canvasGame.clear();
   player.render();
   player.update();
+  updateEnemies();
+  checkGameOver();
+  updateShot();
+  checkCrash();
 }
-
 
 document.onkeydown = function (e) {
   switch (e.keyCode) {
@@ -174,9 +362,13 @@ document.onkeydown = function (e) {
     case 32: // space-bar
       player.down = false;
       player.attack = true;
+      if (player.gunReload) {
+        laserArr.push(new Shot(player.right));
+        player.gunReload = false;
+        setTimeout(() => player.gunReload = true, 1000);
+      }
       break;
   }
-
 };
 
 document.onkeyup = function (e) {
